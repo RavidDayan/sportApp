edaeabc9acf2aa90378ba700c4201fdb2329e19a
Ravid Dayan <dayanravid@gmail.com>
2023-11-18 18:27:17 +0200

    added a get reuest for all programs available

--- frontend/src/pages/ProgramHistory.jsx
+++ frontend/src/pages/ProgramHistory.jsx
@@ -1,6 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 export default ProgramHistory;
 
 function ProgramHistory(){
+    const [programList,setProgramList]=useState();
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await axios.get(
+              "http://localhost:5555/programHistory"
+            );
+            const data = response.data;
+            console.log(data);
+            setProgramList(data);
+          } catch (error) {
+            console.error("Error:", error);
+          }
+        };
+        fetchData();
+      }, []);
     return(
         <h1>program History</h1>
     );
