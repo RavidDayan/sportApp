69d5302e47167dd1b90e56ac7297e2679814d8ed
Ravid Dayan <dayanravid@gmail.com>
2023-11-18 18:10:17 +0200

    added exercise list Get reuquest from server

--- frontend/src/pages/AddNewProgram.jsx
+++ frontend/src/pages/AddNewProgram.jsx
@@ -1,23 +1,24 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import Container from "react-bootstrap/esm/Container";
 
 export default AddNewProgram;
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(
-        "http://localhost:5555/newProgram"
-      );
-      const data = response.data;
-      setProgram(data);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-  fetchData();
-}, []);
+
 function AddNewProgram() {
+  const [exerciseList,setExerciseList]=useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5555/newProgram"
+        );
+        const data = response.data;
+        setExerciseList(data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+  }, []);
   return (<Container>
-    
   </Container>);
 }
