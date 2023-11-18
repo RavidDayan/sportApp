4150133b6a2d7a3526318df534a773bc4819bee1
Ravid Dayan <dayanravid@gmail.com>
2023-11-18 17:54:01 +0200

    added a get request from server for all exercises

--- frontend/src/pages/AddNewProgram.jsx
+++ frontend/src/pages/AddNewProgram.jsx
@@ -1,7 +1,21 @@
+import { useState, useEffect } from "react";
 import Container from "react-bootstrap/esm/Container";
 
 export default AddNewProgram;
-
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5555/newProgram"
+      );
+      const data = response.data;
+      setProgram(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  fetchData();
+}, []);
 function AddNewProgram() {
   return (<Container>
     
